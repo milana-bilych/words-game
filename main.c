0b713f801ff38adc47918f2512609e284ba85f73
Sanchez <125086913+EssVeex@users.noreply.github.com>
2023-07-13 12:38:57 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <stdbool.h>
+#include <locale.h>
+#include <windows.h>
+#include <conio.h>
+#include <ctype.h>
+
+typedef struct {
+
+} User;
+
+void ShowMenu()
+{
+
+}
+
+void printGuesssedWord()
+{
+
+{
+
+}
+
+void drawMan()
+{
+
+}
+
+void ShowScores()
+{
+
+}
+
+void UpdateScore()
+{
+
+}
+
+void ShowWords()
+{
+
+}
+
+User AuthenticateUser()
+{
+
+}
+
+int main()
+{
+
+}
